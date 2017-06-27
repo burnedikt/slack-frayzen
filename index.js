e41700f00c7c258bdb68595f6039f19506a7d045
Benedikt Reiser <benedikt.reiser@gmail.com>
2017-06-27 17:20:21 +0200

    added slack app draft

--- index.js
+++ index.js
@@ -1,80 +1,57 @@
-// dependencies
-const cheerio = require('cheerio');
-const request = require('request-promise-native');
-const dateformat = require('dateformat');
+// const menu = require('./menu');
+// menu.then((meals) => {
+//   console.log(meals);
+// });
 
-// main menu url
-const mensaId = 411;
-const menuUrl = `http://www.studentenwerk-muenchen.de/mensa/speiseplan/speiseplan_${mensaId}_-de.html`;
+const RtmClient = require('@slack/client').RtmClient;
 
-// request the menu
-request(menuUrl)
-  .then((html) => {
-    // parse the html page with cheerio (basically severside jquery)
-    const $ = cheerio.load(html);
-    // find today's menu section
-    const todayStringFormat = dateformat(new Date(), 'yyyy-mm-dd');
-    const meals = $(`.heute_${todayStringFormat}`)
-      .siblings('.c-schedule__list')
-      .children('.c-schedule__list-item')
-      .map((index, element) => {
-        let $ele = $(element);
-        // get the text node only
-        let name = $ele.find('.js-schedule-dish-description')
-          .contents().filter(function(){
-            return this.nodeType == 3;
-          }).text().trim();
-        // check whether the meal is with or without meat
-        let meatless = parseInt($ele.data('essen-fleischlos'), 10);
-        // whether or not the meal is vegan
-        let vegan = false;
-        // whether or not the meal is vegetarian
-        let vegetarian = false;
-        let meatType = null;
-        if (meatless > 0) {
-          // meatless case
-          vegetarian = true;
-          if (meatless > 1) {
-            vegan = true;
-          }
-        } else {
-          // meat case
-          let meatTypeString = $ele.data('essen-typ');
-          switch (meatTypeString) {
-          case 'S':
-            meatType = 'Schweinefleisch';
-            break;
-          case 'R':
-            meatType = 'Rindfleisch';
-          }
-        }
-        // check allergenes
-        let allergenes = $ele.data('essen-allergene').trim();
-        if (allergenes) {
-          allergenes = allergenes.split(',');
-        } else {
-          allergenes = [];
-        }
-        // and additives
-        let additives = $ele.data('essen-zusatz').trim();
-        if (additives) {
-          additives = additives.split(',');
-        } else {
-          additives = [];
-        }
-        // return all gathered information
-        return {
-          'name' : name,
-          'vegan': vegan,
-          'vegetarian': vegetarian,
-          'meatType': meatType,
-          'allergenes': allergenes,
-          'additives': additives
-        };
-      })
-      .toArray();
-    console.log(meals);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// The memory data store is a collection of useful functions we can include in our RtmClient
+const MemoryDataStore = require('@slack/client').MemoryDataStore;
+const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+
+const bot_token = process.env.SLACK_BOT_TOKEN || '';
+
+const rtm = new RtmClient(bot_token, {
+  // Sets the level of logging we require
+  logLevel: 'info',
+  // Initialise a data store for our client, this will load additional helper functions for the storing and retrieval of data
+  dataStore: new MemoryDataStore()
+});
+
+rtm.start();
+
+let user, team, generalChannelId;
+
+// The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload if you want to cache it
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
+  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+
+  // find the ID of the #general channel
+  for (const c of rtmStartData.channels) {
+    if (c.is_member && c.name ==='general') {
+      generalChannelId = c.id;
+    }
+  }
+});
+
+// Wait for the client to connect
+rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
+  // Get the user's name
+  user = rtm.dataStore.getUserById(rtm.activeUserId);
+
+  // Get the team's name
+  team = rtm.dataStore.getTeamById(rtm.activeTeamId);
+
+  // Log the slack team name and the bot's name
+  console.log('Connected to ' + team.name + ' as ' + user.name);
+});
+
+// Handle incoming events
+rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
+  if (message.channel === generalChannelId) {
+    // do not handle messages in #general
+    return;
+  }
+  console.log('Message:', message); //this is no doubt the lamest possible message handler, but you get the idea
+});
---
+++ menu.js
@@ -0,0 +1,82 @@
+// dependencies
+const cheerio = require('cheerio');
+const request = require('request-promise-native');
+const dateformat = require('dateformat');
+
+// main menu url
+const mensaId = 411;
+const menuUrl = `http://www.studentenwerk-muenchen.de/mensa/speiseplan/speiseplan_${mensaId}_-de.html`;
+
+// request the menu
+const menu = request(menuUrl)
+  .then((html) => {
+    // parse the html page with cheerio (basically severside jquery)
+    const $ = cheerio.load(html);
+    // find today's menu section
+    const todayStringFormat = dateformat(new Date(), 'yyyy-mm-dd');
+    const meals = $(`.heute_${todayStringFormat}`)
+      .siblings('.c-schedule__list')
+      .children('.c-schedule__list-item')
+      .map((index, element) => {
+        let $ele = $(element);
+        // get the text node only
+        let name = $ele.find('.js-schedule-dish-description')
+          .contents().filter(function(){
+            return this.nodeType == 3;
+          }).text().trim();
+        // check whether the meal is with or without meat
+        let meatless = parseInt($ele.data('essen-fleischlos'), 10);
+        // whether or not the meal is vegan
+        let vegan = false;
+        // whether or not the meal is vegetarian
+        let vegetarian = false;
+        let meatType = null;
+        if (meatless > 0) {
+          // meatless case
+          vegetarian = true;
+          if (meatless > 1) {
+            vegan = true;
+          }
+        } else {
+          // meat case
+          let meatTypeString = $ele.data('essen-typ');
+          switch (meatTypeString) {
+          case 'S':
+            meatType = 'Schweinefleisch';
+            break;
+          case 'R':
+            meatType = 'Rindfleisch';
+          }
+        }
+        // check allergenes
+        let allergenes = $ele.data('essen-allergene').trim();
+        if (allergenes) {
+          allergenes = allergenes.split(',');
+        } else {
+          allergenes = [];
+        }
+        // and additives
+        let additives = $ele.data('essen-zusatz').trim();
+        if (additives) {
+          additives = additives.split(',');
+        } else {
+          additives = [];
+        }
+        // return all gathered information
+        return {
+          'name' : name,
+          'vegan': vegan,
+          'vegetarian': vegetarian,
+          'meatType': meatType,
+          'allergenes': allergenes,
+          'additives': additives
+        };
+      })
+      .toArray();
+    return meals;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+module.exports = menu;
